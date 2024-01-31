c97c8be7f261e92fe17137b2674ca238f168414c
Vishwa2706 <vishwajayanth3@gmail.com>
2024-01-31 19:42:21 +0530

    .

--- myapp/index.js
+++ myapp/index.js
@@ -2,10 +2,19 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000 
 const cors = require('cors')
+const path = require('path')
+
+// const __dirname = path.resolve();
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "/mern-client/dist")))
+
+app.get("*", (req, res)=>{
+  res.sendFile(path.join(__dirname, "mern-client", "dist", "index.html"));
+})
 
 //mongodb configuration
 
